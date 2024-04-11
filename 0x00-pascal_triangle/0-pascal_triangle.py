fb91ae87acf22405663c67c0f2f8fd913eb12389
othmanoach <othmanoach@gmail.com>
2024-04-11 14:43:13 +0000

    check

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -20,5 +20,4 @@ def pascal_triangle(n):
             else:
                 temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
         triangle.append(temp_list)
-    # print(triangle)
-return triangle
+        return triangle
